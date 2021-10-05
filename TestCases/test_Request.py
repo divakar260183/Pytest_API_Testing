8a9459c3ef662846514a12aa3a0e07da548328ea
Divakar Mishra <divakar.mishra@comm100.com>
2021-10-05 11:08:21 +0530

    elif

--- TestCases/test_Request.py
+++ TestCases/test_Request.py
@@ -7,20 +7,16 @@ def test_api_request(request_url, request_type, request_header, request_param, r
     if is_login_required:
         if request_type == 'GET':
             response = requests.get(request_url, headers=request_header)
-        else:
-            if request_type == 'POST':
-                response = requests.post(request_url, json=request_body, headers=request_header,
-                                         params=request_param)
-            else:
-                if request_type == 'DELETE':
-                    response = requests.delete(request_url, headers=request_header)
+        elif request_type == 'POST':
+            response = requests.post(request_url, json=request_body, headers=request_header,
+                                     params=request_param)
+        elif request_type == 'DELETE':
+            response = requests.delete(request_url, headers=request_header)
     else:
         if request_type == 'GET':
             response = requests.get(request_url)
-        else:
-            if request_type == 'POST':
-                response = requests.post(request_url, json=request_body)
-            else:
-                if request_type == 'DELETE':
-                    response = requests.delete(request_url)
+        elif request_type == 'POST':
+            response = requests.post(request_url, json=request_body)
+        elif request_type == 'DELETE':
+            response = requests.delete(request_url)
     assert response.status_code == response_code, "Response Code is not correct"
