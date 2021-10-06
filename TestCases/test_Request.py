b401cf0288869b84cc7d18022ebd94c26a6d4b3e
Divakar Mishra <divakar.mishra@comm100.com>
2021-10-06 11:04:02 +0530

    validate data initial code

--- TestCases/test_Request.py
+++ TestCases/test_Request.py
@@ -1,19 +1,8 @@
 import json
 
 import requests
-from genson import SchemaBuilder
-from jsonschema import validate
 
-
-def validate_schema(actual_data, expected_data):
-    builder = SchemaBuilder()
-    builder.add_object(expected_data)
-    schema = builder.to_schema()
-    if isinstance(actual_data, list):
-        for data in actual_data:
-            validate(instance=data, schema=schema)
-    else:
-        validate(instance=expected_data, schema=schema)
+from util.utilities import validate_schema, validate_response_data
 
 
 def test_api_request(request_url, request_type, request_header, request_param, request_body,
@@ -34,7 +23,6 @@ def test_api_request(request_url, request_type, request_header, request_param, r
             response = requests.post(request_url, json=request_body)
         elif request_type == 'DELETE':
             response = requests.delete(request_url)
-    print("content :", response.content)
-    print("response schema :", response_schema)
     assert response.status_code == response_code, "Response Code is not correct"
     validate_schema(json.loads(response.content), response_schema)
+    validate_response_data(json.loads(response.content), response_schema)
--- util/utilities.py
+++ util/utilities.py
@@ -1,3 +1,6 @@
+from genson import SchemaBuilder
+from jsonschema import validate
+
 
 def get_value_for_key(data_list, key_value):
     value = None
@@ -7,3 +10,33 @@ def get_value_for_key(data_list, key_value):
                 if key == key_value:
                     value = items[key]
     return value
+
+
+def validate_schema(actual_data, expected_data):
+    builder = SchemaBuilder()
+    if isinstance(expected_data, list):
+        builder.add_object(expected_data[0])
+        schema = builder.to_schema()
+    else:
+        builder.add_object(expected_data)
+        schema = builder.to_schema()
+    if isinstance(actual_data, list):
+        for data in actual_data:
+            validate(instance=data, schema=schema)
+    else:
+        validate(instance=expected_data, schema=schema)
+
+
+def validate_response_data(actual_data, expected_data):
+    if isinstance(expected_data, list):
+        for exp_data in expected_data:
+            for items in exp_data:
+                if exp_data[items] !="XXXX":
+                    if isinstance(actual_data, list):
+                        for data in actual_data:
+                            assert exp_data[items] == data[items]
+                    else:
+                        assert exp_data[items] == actual_data[items]
+
+
+
